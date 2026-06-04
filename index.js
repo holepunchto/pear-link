fb58c6b81933411f248febaf5d04ef285a0a112f
rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>
2026-06-04 18:25:34 +0200

    removed aliases (#16)

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 'use strict'
 const path = require('path')
 const { ERR_INVALID_LINK } = require('pear-errors')
-const { ALIASES } = require('pear-aliases')
 const hid = require('hypercore-id-encoding')
 const FILE = 'file:'
 const PEAR = 'pear:'
 const DOUB = '//'
-
-function decode(v, info = {}) {
-  try {
-    return hid.decode(v)
-  } catch (err) {
-    if (typeof v === 'string') {
-      throw ERR_INVALID_LINK('alias not found: "' + v + '"', { ...info, err })
-    }
-    throw err
-  }
-}
 
 class PearLink {
   normalize(link) {
@@ -32,7 +20,7 @@ class PearLink {
     if (protocol === FILE) return `${protocol}//${pathname}${search}${hash}`
     if (!protocol && drive) protocol = PEAR
     if (protocol === PEAR) {
-      const key = drive.alias || hid.normalize(drive.key)
+      const key = hid.normalize(drive.key)
       const base = [
         drive.fork,
         drive.length,
@@ -58,9 +46,6 @@ class PearLink {
       link.startsWith(PEAR + DOUB) === false &&
       link.startsWith(FILE + DOUB) === false
     const isRelativePath = isPath && link[0] !== '/' && link[1] !== ':'
-    const keys = Object.fromEntries(
-      Object.entries(ALIASES).map(([k, v]) => [hid.encode(v), k])
-    )
     const { protocol, pathname, hostname, search, hash } = isRelativePath
       ? new URL(link, FILE + DOUB + path.resolve('.') + '/')
       : new URL(isPath ? FILE + DOUB + link : link)
@@ -86,16 +71,13 @@ class PearLink {
         }
       }
     } else if (protocol === PEAR) {
-      const [fork, length, keyOrAlias, apphash] = hostname.split('.')
+      const [fork, length, key, apphash] = hostname.split('.')
       const parts = hostname.split('.').length
 
       if (parts === 1) {
-        // pear://keyOrAlias[/some/path]
-        const key = ALIASES[hostname] || decode(hostname, info)
-        const origin = keys[hid.encode(key)]
-          ? `${protocol}//${keys[hid.encode(key)]}`
-          : `${protocol}//${hostname}`
-        const alias = ALIASES[hostname] ? hostname : null
+        // pear://key[/some/path]
+        const key = hostname
+        const origin = `${protocol}//${hostname}`
         return {
           protocol,
           pathname,
@@ -103,11 +85,10 @@ class PearLink {
           hash,
           origin,
           drive: {
-            key,
+            key: hid.decode(key),
             length: null,
             fork: null,
-            hash: null,
-            alias
+            hash: null
           }
         }
       }
@@ -117,14 +98,10 @@ class PearLink {
         throw ERR_INVALID_LINK('Incorrect hostname', info)
       }
 
-      const alias = ALIASES[keyOrAlias] ? keyOrAlias : null
-      const key = ALIASES[keyOrAlias] || decode(keyOrAlias, info)
-      const origin = keys[hid.encode(key)]
-        ? `${protocol}//${keys[hid.encode(key)]}`
-        : `${protocol}//${keyOrAlias}`
+      const origin = `${protocol}//${key}`
 
       if (parts === 3) {
-        // pear://fork.length.keyOrAlias[/some/path]
+        // pear://fork.length.key[/some/path]
         if (!Number.isInteger(+fork) || !Number.isInteger(+length)) {
           throw ERR_INVALID_LINK('Incorrect hostname', info)
         }
@@ -135,17 +112,16 @@ class PearLink {
           hash,
           origin,
           drive: {
-            key,
+            key: hid.decode(key),
             length: Number(length),
             fork: Number(fork),
-            hash: null,
-            alias
+            hash: null
           }
         }
       }
 
       if (parts === 4) {
-        // pear://fork.length.keyOrAlias.dhash[/some/path]
+        // pear://fork.length.key.dhash[/some/path]
         if (!Number.isInteger(+fork) || !Number.isInteger(+length)) {
           throw ERR_INVALID_LINK('Incorrect hostname', info)
         }
@@ -157,11 +133,10 @@ class PearLink {
           hash,
           origin,
           drive: {
-            key,
+            key: hid.decode(key),
             length: Number(length),
             fork: Number(fork),
-            hash: hid.decode(apphash),
-            alias
+            hash: hid.decode(apphash)
           }
         }
       }
